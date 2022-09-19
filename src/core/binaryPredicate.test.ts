3da4bf27e196c2da60b3db11c1704bdecbbcd3af
EvitanRelta <shauntanzongzhi@gmail.com>
2022-09-20 00:28:04 +0800

    Fix binary-predicate tests

--- src/core/binaryPredicate.test.ts
+++ src/core/binaryPredicate.test.ts
@@ -1,43 +1,43 @@
-import { biAll, biAny, biNone, biNot, biOnlyOne } from '.'
+import { all, any, none, not, onlyOne } from '.'
 
 const isEqual = (a: number, b: number) => a === b
 const isBothPositive = (a: number, b: number) => a > 0 && b > 0
 const isBothNot1 = (a: number, b: number) => a !== 1 && b !== 1
 
-test('Binary - biNot', () => {
-    const isNotEqual = biNot(isEqual)
+test('Binary - not', () => {
+    const isNotEqual = not(isEqual)
     expect(isNotEqual(1, 1)).toBe(false)
     expect(isNotEqual(100, 100)).toBe(false)
     expect(isNotEqual(1, 100)).toBe(true)
     expect(isNotEqual(100, 1)).toBe(true)
 })
 
-test('Binary - biAll', () => {
-    const combined = biAll(isEqual, isBothPositive, isBothNot1)
+test('Binary - all', () => {
+    const combined = all(isEqual, isBothPositive, isBothNot1)
     expect(combined(2, 2)).toBe(true)
     expect(combined(1, 1)).toBe(false)
     expect(combined(-2, -2)).toBe(false)
     expect(combined(2, 3)).toBe(false)
 })
 
-test('Binary - biAny', () => {
-    const combined = biAny(isEqual, isBothPositive, isBothNot1)
+test('Binary - any', () => {
+    const combined = any(isEqual, isBothPositive, isBothNot1)
     expect(combined(2, 2)).toBe(true)
     expect(combined(1, 1)).toBe(true)
     expect(combined(-2, -2)).toBe(true)
     expect(combined(-2, 1)).toBe(false)
 })
 
-test('Binary - biNone', () => {
-    const combined = biNone(isEqual, isBothPositive, isBothNot1)
+test('Binary - none', () => {
+    const combined = none(isEqual, isBothPositive, isBothNot1)
     expect(combined(2, 2)).toBe(false)
     expect(combined(1, 1)).toBe(false)
     expect(combined(-2, -2)).toBe(false)
     expect(combined(-2, 1)).toBe(true)
 })
 
-test('Binary - biOnlyOne', () => {
-    const combined = biOnlyOne(isEqual, isBothPositive, isBothNot1)
+test('Binary - onlyOne', () => {
+    const combined = onlyOne(isEqual, isBothPositive, isBothNot1)
     expect(combined(2, 2)).toBe(false)
     expect(combined(1, 1)).toBe(false)
     expect(combined(1, 3)).toBe(true)
